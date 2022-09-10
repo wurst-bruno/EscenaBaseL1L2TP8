2bff801248d640f9debdbe9084837f92eb65caa5
wurst-bruno <bruno.wurstt@gmail.com>
2022-09-10 16:43:10 -0300

    Update DialogoManager.cs

--- Assets/Scripts/DialogoManager.cs
+++ Assets/Scripts/DialogoManager.cs
@@ -1,11 +1,13 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class DialogoManager : MonoBehaviour
 {
 
     [SerializeField] GameObject dialogueUI;
+    [SerializeField] TextMeshProUGUI textoDelDialogo;
 
     // Start is called before the first frame update
     void Start()
@@ -25,6 +27,8 @@ public class DialogoManager : MonoBehaviour
         // al entrar activa la UI de diálogo
         if (other.gameObject.CompareTag("NPC"))
         {
+            textoDelDialogo.text = " Hola forastero ! ";
+
             dialogueUI.SetActive(true);
         }
     }
