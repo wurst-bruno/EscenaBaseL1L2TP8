cb619fab3843a64d326d82dd8cf6a3d37e0bde86
wurst-bruno <bruno.wurstt@gmail.com>
2022-09-10 16:25:07 -0300

    0

---
+++ Assets/Scripts/DialogoManager.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogoManager : MonoBehaviour
+{
+
+    [SerializeField] GameObject dialogueUI;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        dialogueUI.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+
+        // al entrar activa la UI de diálogo
+        if (other.gameObject.CompareTag(" NPC "))
+        {
+            dialogueUI.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        // al salir desactiva la UI de diálogo
+        if (other.gameObject.CompareTag(" NPC "))
+        {
+            dialogueUI.SetActive(false);
+
+        }
+    }
+}
