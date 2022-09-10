4f1f73c33d36011cf62af7f5989968a0abd1c8a5
wurst-bruno <bruno.wurstt@gmail.com>
2022-09-10 16:51:28 -0300

    +

---
+++ Assets/Scripts/Aignaasilla.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Aignaasilla : MonoBehaviour
+{
+
+    [SerializeField] GameObject Silladialogo;
+    [SerializeField] TextMeshProUGUI textoDelDialogo;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Silladialogo.SetActive(false);
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        // al entrar activa la UI de diálogo
+        if (other.gameObject.CompareTag("Silla"))
+        {
+            textoDelDialogo.text = "Press E";
+
+            Silladialogo.SetActive(true);
+        }
+    }
+    void OnTriggerExit(Collider other)
+    {
+        // al salir desactiva la UI de diálogo
+        if (other.gameObject.CompareTag("Silla"))
+        {
+            Silladialogo.SetActive(false);
+        }
+    }
+}
