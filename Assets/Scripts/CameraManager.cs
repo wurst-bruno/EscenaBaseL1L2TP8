b2d333e5af661329ea74195d1a0d1ecb26e0b51e
wurst-bruno <101563441+wurst-bruno@users.noreply.github.com>
2022-09-19 11:58:23 -0300

    Clase 19/9

---
+++ Assets/Scripts/CameraManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraManager : MonoBehaviour
+{
+    public GameObject camintro;
+    public GameObject camPlayer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        camPlayer.SetActive(false);
+        camintro.SetActive(true);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void cameraactivation()
+    {
+        camPlayer.SetActive(true);
+        camintro.SetActive(false);
+    }
+}
---
+++ Assets/Scripts/CameraScript.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraScript : MonoBehaviour
+{
+    public CameraManager camManager;
+   
+    public void activatecamera()
+    {
+        camManager.cameraactivation();
+    }
+}
